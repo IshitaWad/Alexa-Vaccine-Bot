5984f3f30bd8bad91ad8bfd021e1487b450c70c5
IshitaWad <68130331+IshitaWad@users.noreply.github.com>
2022-01-16 11:42:28 -0500

    Update get_available_slots.py

--- get_available_slots.py
+++ get_available_slots.py
@@ -8,17 +8,18 @@ import time
 
 #categories available: AGE12TO17, Communities, Targeted, MODERNASECONDPUBLIC
 
+global DATA_DIR
+global LAST_RUN_JSON
+
 #api-endpoint
 VACCINETO_API_SLOTS="https://vaccineto.ca/api/slots"
 
-def get_open_slots():
+def get_open_slots(time):
     global DATA_DIR
     global LAST_RUN_JSON
-
-    print("")
-    print("----------------------------------------")
-    print("")
-    print(datetime.now().strftime("%Y-%d-%mT%H:%M:%S") + " : Checking for new open slots")
+    
+    counter = 0
+    name = ""
 
     with open(os.path.join(DATA_DIR, LAST_RUN_JSON)) as last_run_json_file:
         last_run_json = json.load(last_run_json_file)
@@ -27,7 +28,7 @@ def get_open_slots():
     full_data = response.json()
     data = full_data["data"]
     parsed_data = {}
-    parsed_data["date_fetched"] = datetime.now().strftime("%Y-%d-%mT%H:%M:%S")
+    parsed_data["date_fetched"] = datetime.now(time).strftime("%Y-%d-%mT%H:%M:%S")
     parsed_data["data"] = {}
 
     for clinic in data:
@@ -52,10 +53,11 @@ def get_open_slots():
         clinic_data["total_open_slots"] = total_open_slots
         parsed_data["data"][clinic] = clinic_data
 
-    date_time_obj = datetime.now()
+    date_time_obj = datetime.now(time)
     output_json_file = "{}.json".format(date_time_obj.strftime("%Y-%d-%m_%H-%M-%S"))
 
     for clinic in parsed_data["data"]:
+
         total_open_slots = parsed_data["data"][clinic]["total_open_slots"]
         if clinic in last_run_json["data"]:
             last_run_total_open_slots = last_run_json["data"][clinic]["total_open_slots"]
@@ -65,12 +67,9 @@ def get_open_slots():
             total_diff_since_last_run = total_open_slots
         parsed_data["data"][clinic]["change_since_last_fetch"] = total_open_slots - last_run_total_open_slots
 
-        if(total_open_slots > 200):
+        counter = 1;
 
-            print("")
-            print("Clinic: {}".format(parsed_data["data"][clinic]["name"]))
-            print("---------------------------------")
-        else:
+        if(total_open_slots < 200):
             continue
 
         for group in parsed_data["data"][clinic]["open_slots"]:
@@ -85,39 +84,18 @@ def get_open_slots():
                     else:
                         diff_since_last_run_slot = available_slots
                     group_summary_msg += "{} : {} ({:+})\n".format(date, available_slots, diff_since_last_run_slot)
-            if are_slots_available:
-                print(group_summary_msg)
         
         if(total_open_slots > 200):
-
-            print("Total open slots for {} : {}".format(parsed_data["data"][clinic]["name"], total_open_slots))
-            print("Change since last fetch for {} : {:+}".format(parsed_data["data"][clinic]["name"], total_diff_since_last_run))
-            print("")
+            name = parsed_data["data"][clinic]["name"]
         else:
             continue
+
+        if counter == 1:
+            break;
 
     with open(os.path.join(DATA_DIR, output_json_file), 'w') as output_json:
         json.dump(parsed_data, output_json, indent=4)
     LAST_RUN_JSON = output_json_file
 
-
-
-def on_exit_handler():
-    os.rename(os.path.join(DATA_DIR, LAST_RUN_JSON), os.path.join(DATA_DIR, "last-run.json"))
-
- 
-if __name__ == "__main__":
-    global DATA_DIR
-    global LAST_RUN_JSON
-    LAST_RUN_JSON = "last-run.json"
-    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-    DATA_DIR = os.path.join(ROOT_DIR, "data")
-
-    atexit.register(on_exit_handler)
-    get_open_slots()
-    schedule.every(3).minutes.do(get_open_slots)
-
-    while 1:
-        schedule.run_pending()
-        time.sleep(1)
+    return name
 
